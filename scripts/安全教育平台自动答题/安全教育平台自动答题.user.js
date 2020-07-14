e38322b579ef9f5d69f1fd748928ffeecf4c4101
unknown <kkocdko@163.com>
2020-07-14 23:23:33 +0800

    Update

--- scripts/安全教育平台自动答题/安全教育平台自动答题.user.js
+++ scripts/安全教育平台自动答题/安全教育平台自动答题.user.js
@@ -2,36 +2,42 @@
 // @name         安全教育平台自动答题
 // @description  支持安全课程、专题活动
 // @namespace    https://greasyfork.org/users/197529
-// @version      0.7.10
+// @version      0.7.12
 // @author       kkocdko
 // @license      Unlicense
 // @match        *://*.xueanquan.com/*
 // ==/UserScript==
 "use strict";
 
+const { addFloatButton } = {
+  addFloatButton(text, onClick) /* 20200707-123713 */ {
+    if (!document.addFloatButton) {
+      const container = document.body
+        .appendChild(document.createElement("div"))
+        .attachShadow({ mode: "open" });
+      container.innerHTML =
+        "<style>:host{position:fixed;top:3px;left:3px;z-index:2147483647;height:0}#i{display:none}*{float:left;margin:4px;padding:1em;outline:0;border:0;border-radius:5px;background:#1e88e5;box-shadow:0 1px 4px rgba(0,0,0,.1);color:#fff;font-size:14px;line-height:0;transition:.3s}:active{background:#42a5f5;box-shadow:0 2px 5px rgba(0,0,0,.2)}button:active{transition:0s}:checked~button{visibility:hidden;opacity:0;transform:translateY(-3em)}label{border-radius:50%}:checked~label{opacity:.3;transform:translateY(3em)}</style><input id=i type=checkbox><label for=i></label>";
+      document.addFloatButton = (text, onClick) => {
+        const button = document.createElement("button");
+        button.textContent = text;
+        button.addEventListener("click", onClick);
+        return container.appendChild(button);
+      };
+    }
+    return document.addFloatButton(text, onClick);
+  },
+};
+
+const isSpecialTopicPage = location.host.split(".")[0] === "huodong";
+
 addFloatButton("自动答题", () => {
   document.querySelectorAll("[type=radio], [type=checkbox]").forEach((el) => {
-    if (!el.checked) {
+    if (isSpecialTopicPage) {
       el.value = 1;
+    }
+    if (!el.checked) {
       el.click();
     }
   });
-  window.scrollTo(window.scrollX, document.scrollingElement.scrollHeight);
+  scroll(scrollX, document.scrollingElement.scrollHeight);
 });
-
-function addFloatButton(text, onclick) {
-  if (!document.addFloatButton) {
-    const buttonContainer = document.body
-      .appendChild(document.createElement("div"))
-      .attachShadow({ mode: "open" });
-    buttonContainer.innerHTML =
-      "<style>:host{position:fixed;top:3px;left:3px;z-index:2147483647;height:0}#i{display:none}*{float:left;margin:4px;padding:1em;outline:0;border:0;border-radius:5px;background:#1e88e5;box-shadow:0 1px 4px rgba(0,0,0,.1);color:#fff;font-size:14px;line-height:0;transition:.3s}:active{background:#42a5f5;box-shadow:0 2px 5px rgba(0,0,0,.2)}button:active{transition:0s}:checked~button{visibility:hidden;opacity:0;transform:translateY(-3em)}label{border-radius:50%}:checked~label{opacity:.3;transform:translateY(3em)}</style><input id=i type=checkbox><label for=i></label>";
-    document.addFloatButton = (text, onclick) => {
-      const button = document.createElement("button");
-      button.textContent = text;
-      button.addEventListener("click", onclick);
-      return buttonContainer.appendChild(button);
-    };
-  }
-  return document.addFloatButton(text, onclick);
-}
--- scripts/转换微信公众号图片到兼容格式/转换微信公众号图片到兼容格式.user.js
+++ scripts/转换微信公众号图片到兼容格式/转换微信公众号图片到兼容格式.user.js
@@ -2,13 +2,32 @@
 // @name         转换微信公众号图片到兼容格式
 // @description  将WEBP格式图片转换到JPEG、PNG等兼容性较好的格式
 // @namespace    https://greasyfork.org/users/197529
-// @version      0.6
+// @version      0.6.1
 // @author       kkocdko
 // @license      Unlicense
 // @match        *://mp.weixin.qq.com/*
 // @inject-into  content
 // ==/UserScript==
 "use strict";
+
+const { addFloatButton } = {
+  addFloatButton(text, onClick) /* 20200707-123713 */ {
+    if (!document.addFloatButton) {
+      const container = document.body
+        .appendChild(document.createElement("div"))
+        .attachShadow({ mode: "open" });
+      container.innerHTML =
+        "<style>:host{position:fixed;top:3px;left:3px;z-index:2147483647;height:0}#i{display:none}*{float:left;margin:4px;padding:1em;outline:0;border:0;border-radius:5px;background:#1e88e5;box-shadow:0 1px 4px rgba(0,0,0,.1);color:#fff;font-size:14px;line-height:0;transition:.3s}:active{background:#42a5f5;box-shadow:0 2px 5px rgba(0,0,0,.2)}button:active{transition:0s}:checked~button{visibility:hidden;opacity:0;transform:translateY(-3em)}label{border-radius:50%}:checked~label{opacity:.3;transform:translateY(3em)}</style><input id=i type=checkbox><label for=i></label>";
+      document.addFloatButton = (text, onClick) => {
+        const button = document.createElement("button");
+        button.textContent = text;
+        button.addEventListener("click", onClick);
+        return container.appendChild(button);
+      };
+    }
+    return document.addFloatButton(text, onClick);
+  },
+};
 
 addFloatButton("转换全部图片到兼容格式", function () {
   document.querySelectorAll("img").forEach((el) => {
@@ -23,20 +42,3 @@ addFloatButton("转换全部图片到兼容格式", function () {
   this.style.background = "#4caf50";
   this.textContent = "全部图片都已转换完成！";
 });
-
-function addFloatButton(text, onclick) {
-  if (!document.addFloatButton) {
-    const buttonContainer = document.body
-      .appendChild(document.createElement("div"))
-      .attachShadow({ mode: "open" });
-    buttonContainer.innerHTML =
-      "<style>:host{position:fixed;top:3px;left:3px;z-index:2147483647;height:0}#i{display:none}*{float:left;margin:4px;padding:1em;outline:0;border:0;border-radius:5px;background:#1e88e5;box-shadow:0 1px 4px rgba(0,0,0,.1);color:#fff;font-size:14px;line-height:0;transition:.3s}:active{background:#42a5f5;box-shadow:0 2px 5px rgba(0,0,0,.2)}button:active{transition:0s}:checked~button{visibility:hidden;opacity:0;transform:translateY(-3em)}label{border-radius:50%}:checked~label{opacity:.3;transform:translateY(3em)}</style><input id=i type=checkbox><label for=i></label>";
-    document.addFloatButton = (text, onclick) => {
-      const button = document.createElement("button");
-      button.textContent = text;
-      button.addEventListener("click", onclick);
-      return buttonContainer.appendChild(button);
-    };
-  }
-  return document.addFloatButton(text, onclick);
-}
