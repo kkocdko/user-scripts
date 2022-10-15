75bc7cefb6be2771476037cbc2612baa70801620
kkocdko <kkocdko@163.com>
2022-10-15 13:15:13 +0800

    deprecated ojcn-report-gen

--- scripts/ojcn-report-gen/code.user.js
+++ scripts/ojcn-report-gen/code.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        OpenJudge Report Gen
+// @name        OJCN Report Gen
 // @description Generate homework report PDF automatically.
 // @description:en Generate homework report PDF automatically.
 // @description:zh-CN 自动生成作业报告 PDF。
@@ -11,6 +11,7 @@
 // ==/UserScript==
 "use strict";
 
+setTimeout(() => location.reload(), 2000);
 // https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.es.min.js
 
 const { addFloatButton, load } = {
@@ -42,34 +43,64 @@ const { addFloatButton, load } = {
       normal: `https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.3.0-beta.2/fonts/Roboto/Roboto-Regular.ttf`,
       bold: `https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.3.0-beta.2/fonts/Roboto/Roboto-Medium.ttf`,
     },
+
+    // https://cdn.jsdelivr.net/npm/@fontsource/noto-sans-sc@4.5.12/files/noto-sans-sc-all-400-normal.woff
   });
   const pid = "01";
-  const pdf = pdfMake.createPdf({
+  const dd = {
     defaultStyle: { font: "Roboto" },
     content: [
       {
         text: `Homework ${pid}`,
-        fontSize: 20,
+        fontSize: 18,
+        bold: true,
         alignment: "center",
-        margin: [0, 20, 0, 10],
+        margin: [0, 28, 0, 24],
       },
 
+      // {
+      //   text: [
+      //     {
+      //       text: "Student ID: ",
+      //       fontSize: 12,
+      //       bold: true,
+      //     },
+      //     { text: "Text ", color: "green" },
+      //     {
+      //       text: "Name: ",
+      //       fontSize: 12,
+      //       bold: true,
+      //     },
+      //     { text: "Text ", color: "blue" },
+      //   ],
+      // },
       {
-        layout: "noBorders", // optional
+        layout: "noBorders",
+        fontSize: 12,
         table: {
-          // headers are automatically repeated if the table spans over multiple pages
-          // you can declare how many rows should be treated as headers
           headerRows: 1,
-          widths: ["auto", "*"],
+          widths: ["auto", 150, "auto", 150],
+
           body: [
             [
               {
-                text: `Problem ${pid}`,
-                fontSize: 16,
+                text: "Student ID:",
                 bold: true,
-                margin: [0, 0, 0, 18],
+                margin: [32, 0, 10, 0],
               },
-              "",
+              {
+                text: "  212210711114".padEnd(20, " "),
+                decoration: "underline",
+              },
+              {
+                text: "Name:",
+                bold: true,
+                margin: [32, 0, 10, 0],
+              },
+              {
+                text: "  KK".padEnd(20, " "),
+                decoration: "underline",
+              },
             ],
           ],
         },
@@ -82,6 +113,9 @@ const { addFloatButton, load } = {
       },
     ],
     footer: (cur, len) => ({ text: `${cur} / ${len}`, alignment: "center" }),
-  });
-  pdf.download();
+  };
+  const pdf = pdfMake.createPdf(dd);
+  // pdf.download();
+  const iframe = document.querySelector("iframe");
+  iframe.src = URL.createObjectURL(await pdf.getBlob());
 })();
