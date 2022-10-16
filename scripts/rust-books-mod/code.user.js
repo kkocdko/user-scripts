116eedba6be47c6c85f2116fcb9faadead276a06
kkocdko <kkocdko@163.com>
2022-10-16 17:42:38 +0800

    update rust-books-mod

--- scripts/rust-books-mod/code.user.js
+++ scripts/rust-books-mod/code.user.js
@@ -1,9 +1,47 @@
 // ==UserScript==
 // @name        Rust Books Mod
 // @match       *://doc.rust-lang.org/*
-// @version     0.1
+// @match       *://docs.rs/*
+// @version     0.2
 // ==/UserScript==
-document.querySelector("#ayu.theme").click();
+"use strict";
+
+const { sleep, waitUntil } = {
+  sleep(ms) /* 20210416-2319 */ {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  },
+  waitUntil(condition, interval = 200, timeout = 5000) /* 20210416-2319 */ {
+    return new Promise((resolve) => {
+      const finish = () => [clearInterval(timer), resolve()];
+      const timer = setInterval(() => condition() && finish(), interval);
+      setTimeout(finish, timeout);
+    });
+  },
+};
+
+// docs.rs
+document.lastChild.appendChild(document.createElement("style")).textContent = `
+html {
+  --color-background: #000;
+  --main-background-color: #000;
+  --sidebar-background-color: #000;
+  --code-block-background-color: #000;
+  --scrollbar-track-background-color: transparent;
+  --scrollbar-thumb-background-color: #7777;
+  --scrollbar-color: #7777 #7777;
+}
+`.replace(/;/g, "!important;");
+waitUntil(() => document.readyState === "complete").then(async () => {
+  if (document.documentElement.dataset.theme === "ayu") return;
+  document.querySelector("#settings-menu").click();
+  await sleep(300);
+  document.querySelector("#preferred-dark-theme-ayu").click();
+  await sleep(300);
+  document.querySelector("#settings-menu").click();
+});
+
+// doc.rust-lang.org and other books
+document.querySelector("#ayu.theme")?.click();
 document.lastChild.appendChild(document.createElement("style")).textContent = `
 .ayu {
   --fg: #eee;
