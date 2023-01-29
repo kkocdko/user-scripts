af5d649c7c0f51271d95cefe8aee20d8374c0db8
kkocdko <kkocdko@163.com>
2023-01-29 09:30:53 +0800

    Update bilibili-pure: fit new version of page

--- scripts/bilibili-pure/code.user.js
+++ scripts/bilibili-pure/code.user.js
@@ -4,7 +4,7 @@
 // @description Pure & Native
 // @description:zh-CN 纯净 & 原生
 // @namespace   https://greasyfork.org/users/197529
-// @version     0.4.1
+// @version     0.4.2
 // @author      kkocdko
 // @license     Unlicense
 // @match       *://*.bilibili.com/robots.txt
@@ -16,55 +16,45 @@
 if (location.pathname !== "/robots.txt") {
   // TODO: use origin part info and cid
   const bvid = location.href.match(/BV.+?(?=(\?|\/|$))/)[0];
+  // localStorage.blPureData = JSON.stringify(__playinfo__.data.dash);
   // __INITIAL_STATE__.p;
   location = "https://www.bilibili.com/robots.txt#" + bvid;
   throw "jump to clean page";
 }
 
-document.head.appendChild(document.createElement("style")).textContent = `
-body { display: flex; justify-content: center; align-items: center; margin: 0; height: 100vh; overflow: hidden; }
-body > :not(video) { display: none; }
-video { max-width: 100%; max-height: 100%; outline: none; }
-`;
+document.head.insertAdjacentHTML(
+  "beforeend",
+  `
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+  <style>
+    body { display: flex; justify-content: center; align-items: center; margin: 0; height: 100vh; overflow: hidden; }
+    body > :not(video) { display: none; }
+    video { max-width: 100%; max-height: 100%; outline: none; }
+  </style>
+  `
+);
 
-/*
-https://api.bilibili.com/x/player/pagelist?bvid=BV1z3411M7Uc
-https://api.bilibili.com/x/player/playurl?qn=16&fnver=0&fnval=4048&bvid=${bvid}&cid=${cid}
-https://api.bilibili.com/x/player/playurl?avid=733986366&cid=927131247&qn=16&type=mp4&platform=html5
-*/
-
-// TODO: optimize, don't get all video info
-const getInfo = (bvid) =>
-  fetch(`https://api.bilibili.com/x/player/pagelist?bvid=${bvid}`)
-    .then((response) => response.json())
-    .then((json) =>
-      json.data.map((videoPart) =>
-        fetch(
-          `https://api.bilibili.com/x/player/wbi/playurl?qn=16&fnver=0&fnval=4048&bvid=${bvid}&cid=${videoPart.cid}`
-        )
-          .then((response) => response.json())
-          .then((json) => ({ title: videoPart.part, ...json.data.dash }))
-      )
-    )
-    .then((tasks) => Promise.all(tasks));
-
-const getTitle = (bvid) =>
-  fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`)
-    .then((r) => r.json())
-    .then((d) => d.data.title);
-
-// TODO: multi part and serials
+// TODO: show parts and serials list
 (async () => {
-  let [bvid, p] = location.hash.slice(1).split(",");
-  p = (p ?? 1) - 1;
-
-  getTitle(bvid).then((title) => {
-    document.title = title + " - Bilibili Pure";
-  });
-
-  const ret = await getInfo(bvid);
-  // const ret = await get("BV1aD4y1h7Yk");
-
+  const [bvid, partid] = location.hash.slice(1).split(",");
+  const opusInfoUrl = `https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`;
+  const opusInfo = (await fetch(opusInfoUrl).then((r) => r.json())).data;
+  document.title = opusInfo.title + " - Bilibili Pure";
+  // const cid = opusInfo.pages[(partid ?? 1) - 1].cid;
+  // const playInfoUrl = `https://api.bilibili.com/x/player/wbi/playurl?avid=${opusInfo.aid}&bvid=${bvid}&cid=${cid}&qn=16&fnver=0&fnval=4048&fourk=1&session=4be331b4864f323a30ed1b4d4b354c38&w_rid=a936d63d4113211db5e98cfe643de29a&wts=1674949149`;
+  // const playInfo = (await fetch(playInfoUrl).then((r) => r.json())).data.dash;
+  // if (!localStorage.blPureData)
+  //   location = `https://www.bilibili.com/video/${bvid}`;
+  // const playInfo = JSON.parse(localStorage.blPureData);
+  // localStorage.blPureData = "";
+  const pageUrl = `https://www.bilibili.com/video/${bvid}`;
+  const page = await fetch(pageUrl).then((v) => v.text());
+  const pageI1 = page.indexOf("__playinfo__=");
+  const pageI2 = page.indexOf("</script>", pageI1);
+  const playInfoText = page.slice(pageI1 + "__playinfo__=".length, pageI2);
+  const playInfo = eval("(" + playInfoText + ")").data.dash;
+  // console.log(eval("(" + playInfoText + ")"));
   const minOf = (arr, f = (e) => e) => {
     let ret = null;
     for (const e of arr) if (ret === null || f(e) < f(ret)) ret = e;
@@ -72,20 +62,28 @@ const getTitle = (bvid) =>
   };
 
   // TODO: HD
-  const aInfo = minOf(ret[p].audio, (e) => e.bandwidth);
-  const vInfo = minOf(ret[p].video, (e) => (e.codecid === 7 ? -e.width : 9e9));
+  const aInfo = minOf(playInfo.audio, (e) => e.bandwidth);
+  const vInfo = minOf(playInfo.video, (e) =>
+    e.codecid === 7 ? -e.width : 9e9
+  );
 
   const audio = document.createElement("audio");
   audio.src = aInfo.baseUrl;
   const video = document.createElement("video");
   video.src = vInfo.baseUrl;
   video.controls = true;
-  video.onplay = () => audio.play();
-  video.onpause = () => audio.pause();
+  video.onplay = () => {
+    audio.play();
+  };
+  video.onpause = () => {
+    audio.pause();
+  };
   video.onratechange = () => {
     audio.playbackRate = video.playbackRate;
   };
-  video.onseeking = () => audio.pause();
+  video.onseeking = () => {
+    audio.pause();
+  };
   let lastLostSync = 0;
   const sync = () => {
     const diff = Math.abs(audio.currentTime - video.currentTime); // unit: seconds
@@ -113,12 +111,3 @@ const getTitle = (bvid) =>
   document.body.innerHTML = "";
   document.body.appendChild(video);
 })();
-
-// (
-//   await fetch(
-//     "https://api.bilibili.com/x/player/wbi/playurl?avid=649205861&bvid=BV1ue4y1j7CK&cid=936203037&qn=16&fnver=0&fnval=4048&fourk=1&session=xxx&w_rid=xxx&wts=1671964872",
-//     {
-//       credentials: "include",
-//     }
-//   ).then((v) => v.json())
-// ).data.dash;
