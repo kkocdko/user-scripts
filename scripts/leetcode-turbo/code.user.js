ade9450129a8e7d83993146be83ed9ab2c545da4
kkocdko <kkocdko@163.com>
2023-11-15 17:04:47 +0800

    Update leetcode-turbo: remove unused code

--- scripts/leetcode-turbo/code.user.js
+++ scripts/leetcode-turbo/code.user.js
@@ -2,7 +2,7 @@
 // @name        LeetCode Turbo
 // @description Replace monaco with vanilla textarea.
 // @namespace   https://greasyfork.org/users/197529
-// @version     0.1.3
+// @version     0.1.4
 // @author      kkocdko
 // @license     Unlicense
 // @match       *://leetcode.com/problems/*
@@ -14,65 +14,37 @@
 const globalThis = this.unsafeWindow || this;
 const originFetch = globalThis.fetch;
 const textarea = document.createElement("textarea");
-// textarea.className =
-//   "monaco-editor no-user-select  showUnused showDeprecated vs";
-// textarea.style =
-//   "font-family: monospace; height: calc(100% + 34px); width: 100%; padding: 6px 10px; white-space: pre; outline: none; margin-top: -34px; ";
-// const replaceEditorTimer = setInterval(() => {
-//   try {
-//     document
-//       .querySelector("#editor")
-//       .children[0].children[1].children[0].replaceWith(textarea);
-//     clearInterval(replaceEditorTimer);
-//   } catch (_) {}
-// }, 500);
 textarea.style =
   "font-family: monospace; height: 100%; width: 100%; padding: 6px 10px; white-space: pre; outline: none;";
 const replaceEditorTimer = setInterval(() => {
-  try {
-    document.querySelector("#editor").replaceWith(textarea);
-    clearInterval(replaceEditorTimer);
-  } catch (_) {}
+  const el = document.querySelector("#editor");
+  if (el) el.replaceWith(textarea), clearInterval(replaceEditorTimer);
 }, 500);
 globalThis.fetch = (input, init) => {
-  if (input?.includes("/lc-monaco/") || input?.includes("/monaco-tm/")) {
+  if (input?.includes("/lc-monaco/") || input?.includes("/monaco-tm/"))
     throw Error("Monaco editor blocked.");
-  }
-  if (input?.endsWith("/submit") || input?.endsWith("/submit/")) {
+  if (input?.endsWith("/submit") || input?.endsWith("/submit/"))
     init.body = JSON.stringify({
       ...JSON.parse(init.body),
       typed_code: textarea.value,
     });
-  }
   return originFetch(input, init);
 };
-const titleSlug = location.href.split("/problems/")[1].split("/")[0];
 fetch("https://leetcode.cn/graphql/", {
-  headers: { "content-type": "application/json" },
   method: "POST",
+  headers: { "content-type": "application/json" },
   body: JSON.stringify({
     operationName: "questionEditorData",
-    variables: { titleSlug },
-    query: `
-      query questionEditorData($titleSlug: String!) {
-        question(titleSlug: $titleSlug) {
-          codeSnippets {
-            langSlug
-            code
-          }
-        }
-      }
-    `,
+    variables: { titleSlug: location.pathname.split("/")[2] },
+    query:
+      "query questionEditorData($titleSlug: String!) { question(titleSlug: $titleSlug) { codeSnippets { langSlug code } } }",
   }),
-})
-  .then((v) => v.json())
-  .then((v) => {
-    textarea.value = v.data.question.codeSnippets.find(
-      (v) => v.langSlug == "cpp"
-    ).code;
-  });
-// just block the requestAnimationFrame is ok?
-globalThis.requestAnimationFrame = () => {};
+}).then(async (v) => {
+  const snippets = (await v.json()).data.question.codeSnippets;
+  textarea.value = snippets.find((v) => v.langSlug == "cpp").code;
+});
+globalThis.requestAnimationFrame = () => {}; // just ignore the requestAnimationFrame is ok?
+
 // make a LRU cache for getComputedStyle
 /*
 const cache4gcs = new Map();
