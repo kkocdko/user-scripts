a6d72154f301c3262bd5be5285b9c175495a9355
kkocdko <kkocdko@163.com>
2024-06-15 10:03:01 +0800

    many-mods: Bump version

--- scripts/many-mods/code.user.js
+++ scripts/many-mods/code.user.js
@@ -2,7 +2,7 @@
 // @name        Many Mods
 // @description Many many small modify for many sites.
 // @namespace   https://greasyfork.org/users/197529
-// @version     2.0.3
+// @version     2.0.4
 // @author      kkocdko
 // @license     Unlicense
 // @match       *://*/*
