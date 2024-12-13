49c2a1d03f785535f5eb3dd723b712347b789d63
kkocdko <kkocdko@gmail.com>
2024-12-14 01:19:56 +0800

    vercel-delete-deployments: Fit new version of vercel console

--- scripts/vercel-delete-deployments/code.user.js
+++ scripts/vercel-delete-deployments/code.user.js
@@ -1,9 +1,8 @@
 document
   .querySelectorAll('div[data-testid="deployment-entity/target"] a')
   .forEach((el) => {
-    el.style.display = "inline-flex";
     const btn = el.parentNode.appendChild(document.createElement("button"));
-    btn.style.float = "right";
+    btn.style = "position:absolute;inset:0 0 0 auto;z-index:128";
     btn.textContent = "DEL";
     btn.onclick = () => {
       const id = el.href.split("/").pop();
